a572faa1f78883926da7377350cc66d42a41906d
Stephen Prill <stephen.prill@gmail.com>
2014-12-05 10:47:48 -0700

    clubs controller spec passing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,7 @@
 class ApplicationController < ActionController::Base
+
   protect_from_forgery with: :exception
+
 
   def current_student
     Student.find_by_id(session[:student_id])
--- app/controllers/clubs_controller.rb
+++ app/controllers/clubs_controller.rb
@@ -1,11 +1,21 @@
 class ClubsController < ApplicationController
 
+  before_action :require_login
+
   def index
     @clubs = Club.all
   end
 
   def show
     @club = Club.find(params[:id])
+  end
+
+  private
+
+  def require_login
+    unless(current_student)
+    redirect_to(login_path)
+    end
   end
 
 end
